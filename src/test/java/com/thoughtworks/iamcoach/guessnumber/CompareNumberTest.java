4a1b52c8e644755c94cb28bcae07835b127577c1
SnakerWorker <snakerworker@gmail.com>
2014-11-01 13:18:34 +0800

    add test should_result_is_4A0B_when_input_isOk

--- src/test/java/com/thoughtworks/iamcoach/guessnumber/CompareNumberTest.java
+++ src/test/java/com/thoughtworks/iamcoach/guessnumber/CompareNumberTest.java
@@ -1,16 +1,28 @@
 package com.thoughtworks.iamcoach.guessnumber;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
 public class CompareNumberTest {
+
+    private CompareNumber compareNumber;
+    @Before
+    public void init(){
+        compareNumber = new CompareNumber();
+    }
+
     @Test
     public void should_result_length_is_4(){
-
-        CompareNumber compareNumber = new CompareNumber();
         String result = compareNumber.compare("xxxx","xxxx");
 
         assertThat(result.length()).isEqualTo(4);
+    }
+
+    @Test
+    public void should_result_is_4A0B_when_input_isOk(){
+        String result = compareNumber.compare("1234","1234");
+        assertThat(result).isEqualTo("4A0B");
     }
 }

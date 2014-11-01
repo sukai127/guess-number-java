84f61c43fdcc51ca1e39389ce2b0ac10691fcc8c
SnakerWorker <snakerworker@gmail.com>
2014-11-01 13:59:03 +0800

    add test should_result_is_no_repeate

--- src/main/java/com/thoughtworks/iamcoach/guessnumber/AnswerGenerator.java
+++ src/main/java/com/thoughtworks/iamcoach/guessnumber/AnswerGenerator.java
@@ -2,6 +2,6 @@ package com.thoughtworks.iamcoach.guessnumber;
 
 public class AnswerGenerator {
     public String generate() {
-        return "1234";
+        return "3333";
     }
 }
--- src/test/java/com/thoughtworks/iamcoach/guessnumber/AnswerGeneratorTest.java
+++ src/test/java/com/thoughtworks/iamcoach/guessnumber/AnswerGeneratorTest.java
@@ -1,19 +1,45 @@
 package com.thoughtworks.iamcoach.guessnumber;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
 public class AnswerGeneratorTest{
 
+    private AnswerGenerator answerGenerator;
+    private String result;
+
+    @Before
+    public void init(){
+       answerGenerator = new AnswerGenerator();
+    }
+
     @Test
     public void should_result_length_be_4(){
 
-        AnswerGenerator answerGenerator = new AnswerGenerator();
-        String result = answerGenerator.generate();
+        result = answerGenerator.generate();
 
         assertThat(result.length()).isEqualTo(4);
 
+    }
+
+    @Test
+    public void should_result_is_no_repeate(){
+
+        result = answerGenerator.generate();
+        boolean isRepeate = false;
+        String temp = "";
+
+        for (int i = 0; i < result.length(); i++){
+           if(temp.contains(result.charAt(i)+"")){
+               isRepeate = true;
+           }else {
+               temp += result.charAt(i);
+           }
+        }
+
+        assertThat(isRepeate).isEqualTo(false);
     }
 
 }

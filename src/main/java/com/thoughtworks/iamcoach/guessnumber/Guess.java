934c3d801226a75f10377bd6d0df2811d8dea72e
SnakerWorker <snakerworker@gmail.com>
2014-11-01 15:21:49 +0800

    add test should_return_correct_result

---
+++ src/main/java/com/thoughtworks/iamcoach/guessnumber/Guess.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.iamcoach.guessnumber;
+
+public class Guess {
+
+    private AnswerGenerator answerGenerator;
+    private CompareNumber compareNumber;
+
+    public Guess(AnswerGenerator answerGenerator, CompareNumber compareNumber) {
+        this.answerGenerator = answerGenerator;
+        this.compareNumber = compareNumber;
+    }
+
+    public String judge(String input) {
+        return null;
+    }
+}
--- src/test/java/com/thoughtworks/iamcoach/guessnumber/GuessTest.java
+++ src/test/java/com/thoughtworks/iamcoach/guessnumber/GuessTest.java
@@ -1,4 +1,26 @@
 package com.thoughtworks.iamcoach.guessnumber;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class GuessTest {
+
+    private Guess guess;
+    @Before
+    public void init(){
+        AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
+        when(answerGenerator.generate()).thenReturn("1234");
+        CompareNumber compareNumber = mock(CompareNumber.class);
+        when(compareNumber.compare("1234","1243")).thenReturn("2A2B");
+        guess = new Guess(answerGenerator,compareNumber);
+    }
+
+    @Test
+    public void should_return_correct_result(){
+        assertThat(guess.judge("1243")).isEqualTo("2A2B");
+    }
 }

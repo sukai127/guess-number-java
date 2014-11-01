1766a57e2ccc3492323c4e8dd83f6acef17f534f
SnakerWorker <snakerworker@gmail.com>
2014-11-01 14:15:55 +0800

    midy generate

--- src/main/java/com/thoughtworks/iamcoach/guessnumber/AnswerGenerator.java
+++ src/main/java/com/thoughtworks/iamcoach/guessnumber/AnswerGenerator.java
@@ -2,6 +2,19 @@ package com.thoughtworks.iamcoach.guessnumber;
 
 public class AnswerGenerator {
     public String generate() {
-        return "3245";
+
+        String result = "";
+
+        int element = (int)(Math.random()*10);
+
+        for (int i = 0;result.length() < 4; i++){
+            if(!result.contains(element+"")){
+                result += element;
+            }else{
+                element = (int)(Math.random()*10);
+            }
+        }
+
+        return result;
     }
 }

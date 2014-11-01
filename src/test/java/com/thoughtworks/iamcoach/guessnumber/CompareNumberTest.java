680910ac87514eec8c831b35e332fc2f52eafa14
SnakerWorker <snakerworker@gmail.com>
2014-11-01 13:26:23 +0800

    modify CompareNumberTest

--- src/test/java/com/thoughtworks/iamcoach/guessnumber/CompareNumberTest.java
+++ src/test/java/com/thoughtworks/iamcoach/guessnumber/CompareNumberTest.java
@@ -8,39 +8,41 @@ import static org.fest.assertions.api.Assertions.assertThat;
 public class CompareNumberTest {
 
     private CompareNumber compareNumber;
+    private String result;
     @Before
     public void init(){
         compareNumber = new CompareNumber();
+
     }
 
     @Test
     public void should_result_length_is_4(){
-        String result = compareNumber.compare("xxxx","xxxx");
+        result = compareNumber.compare("1234","1234");
 
         assertThat(result.length()).isEqualTo(4);
     }
 
     @Test
     public void should_result_is_4A0B_when_input_isOk(){
-        String result = compareNumber.compare("1234","1234");
+        result = compareNumber.compare("1234","1234");
         assertThat(result).isEqualTo("4A0B");
     }
 
     @Test
     public void should_result_is_0A0B(){
-        String result = compareNumber.compare("1234","5678");
+        result = compareNumber.compare("1234","5678");
         assertThat(result).isEqualTo("0A0B");
     }
 
     @Test
     public void should_result_is_2A2B(){
-        String result = compareNumber.compare("1234","1432");
+        result = compareNumber.compare("1234","1432");
         assertThat(result).isEqualTo("2A2B");
     }
 
     @Test
     public void should_result_is_0A4B(){
-        String result = compareNumber.compare("1234","4321");
+        result = compareNumber.compare("1234","4321");
         assertThat(result).isEqualTo("0A4B");
     }
 
